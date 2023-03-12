01a23a08bda0a04cf76eb74d5baef04d8a9de850
Viluron <markusfiederling@gmail.com>
2023-03-12 10:57:18 +0100

    Cleanup

--- src-tauri/src/https.rs
+++ src-tauri/src/https.rs
@@ -1,1 +1,21 @@
+use reqwest::blocking::{Client, Response};
 
+pub fn query(
+    url: &str,
+    username: &str,
+    password: Option<String>,
+    namespace: &str,
+    query: &str,
+) -> Response {
+    let client = Client::new();
+    let res = client
+        .post(url.to_owned() + "/sql")
+        .body(query.to_owned())
+        .basic_auth(username, password)
+        .header("Accept", "application/json")
+        .header("NS", namespace)
+        .send()
+        .unwrap();
+
+    res
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,37 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use reqwest::blocking::Client;
-use reqwest::StatusCode;
-
 mod https;
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 #[tauri::command]
 fn login(url: &str, username: &str, password: Option<String>, namespace: &str) -> bool {
-    let client = Client::new();
-    let res = client
-        .post(url.to_owned() + "/sql")
-        .body("INFO FOR KV;")
-        .basic_auth(username, password)
-        .header("Accept", "application/json")
-        .header("NS", namespace)
-        .send()
-        .unwrap();
+    let response = https::query(
+        &url,
+        &username,
+        password.clone(),
+        &namespace,
+        "INFO FOR KV;",
+    );
 
-    let success = res.status() == StatusCode::OK;
-
-    return success;
+    response.status().is_success()
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![login, greet])
+        .invoke_handler(tauri::generate_handler![login])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
