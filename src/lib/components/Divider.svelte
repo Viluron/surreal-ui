4aad8c6764b6d1e595d9fe8c37d272011000fe0f
Viluron <markusfiederling@gmail.com>
2023-03-13 21:06:26 +0100

    Added Divider Component

---
+++ src/lib/components/Divider.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let width: string = '90%';
+	export let height: string = '1rem';
+</script>
+
+<div class="divider" style={`height: ${height}`}>
+	<div class="line" style={`width: ${width}`} />
+</div>
+
+<style scoped>
+	.divider {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+	}
+
+	.line {
+		height: 1px;
+		background-color: var(--lines);
+	}
+</style>
